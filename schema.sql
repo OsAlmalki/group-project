d71311311a679a8e5ad8ac3e97b08a7b68c43a51
Osama Almalki <osama.moh711@icloud.com>
2025-11-22 10:35:04 +0300

    Delete schema.sql

--- schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS events (
-                                      id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                      title TEXT NOT NULL,
-                                      description TEXT,
-                                      location TEXT,
-                                      date TEXT,
-                                      image TEXT
-);
